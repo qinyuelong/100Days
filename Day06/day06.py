ba9e40be167515a355b731e916d24928d7c3bed4
yuelong qin <qinyuelong.cn@gmail.com>
2020-01-11 09:59:18 +0800

    完成day06 内容

--- Day06/day06.py
+++ Day06/day06.py
@@ -30,4 +30,52 @@ def c_mn():
 
 
 def gcd(x, y):
-    
+    '''最大公约数'''
+    (x, y) = (y, x) if x > y else (x, y)
+    for factor in range(x, 0, -1):
+        if x % factor == 0 and y % factor == 0:
+            return factor
+
+
+
+def lcm(x, y):
+    '''求最小公倍数'''
+    return x * y // gcd(x, y)
+
+
+
+'''
+
+实现判断一个数是不是回文数的函数。
+
+'''
+
+def is_palindrome(num):
+    temp = num
+    total = 0
+    while temp > 0:
+        total = total * 10 + temp % 10
+        temp //= 10
+    return total == num
+
+
+'''
+素数
+指在大於1的自然数中，除了1和該数自身外，無法被其他自然数整除的数
+'''
+
+def is_prime(num):
+    '''判断是不是素数'''
+    for factor in range(2, num):
+        if num % factor == 0:
+            return False
+    return True if num != 1 else False
+
+
+
+if __name__ == '__main__':
+    # num = int(input('请输入正整数: '))
+    for num in range(1, 1000):
+        if is_palindrome(num) and is_prime(num):
+            print('%d 是回文素数' % num)
+
